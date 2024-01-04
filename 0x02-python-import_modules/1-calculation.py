820bb55af945f4421719ef9126ac70018c8ec557
abdelakhliij <riadakhlij@gmail.com>
2024-01-04 11:10:12 -0800

    updated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, subtract, multiply, divide
     a = 10
     b = 5
     print(f"{a} + {b} = {a+b}")
