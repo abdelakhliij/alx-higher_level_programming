3c4607720cc3366329dda19f929bc7fac9ad0723
abdelakhliij <riadakhlij@gmail.com>
2024-01-23 14:56:55 -0800

    updated

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """The class to create square."""
+
+    def __init__(self, size):
+        """Initialzes size to piV
+
+        Args:
+            size (int): the size of the square
+        """
+        self.__size = size
